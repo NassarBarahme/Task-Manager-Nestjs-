3ae550384eb13f445335ee133632369ae7f5d17b
NassarBarahme <nassarbarahme@gmail.com>
2026-01-27 23:21:21 +0200

    add swagger documentaion

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,58 @@
-import { NestFactory } from '@nestjs/core';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { Logger } from '@nestjs/common';
-async function bootstrap() {
-  const logger = new Logger();
+import {
+  ClassSerializerInterceptor,
+  ValidationPipe,
+  Logger,
+} from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+async function bootstrap(): Promise<void> {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'debug', 'verbose'],
   });
 
+  // Global Pipes and Interceptors
   app.useGlobalPipes(new ValidationPipe());
-  const port = 3000;
+
+  const reflector = app.get(Reflector);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
+  // Swagger Configuration
+  const config = new DocumentBuilder()
+    .setTitle('Task Management API')
+    .setDescription(' Nassar Barahmeh - Task Management API')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT || 3000;
   await app.listen(port);
-  logger.log(`Application is running on port ${port}`);
+
+  logger.log(`Application listening on port ${port}`);
+  logger.log(
+    `Swagger documentation available at: http://localhost:${port}/api`,
+  );
 }
-void bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Error during bootstrap:', err);
+  process.exit(1);
+});
